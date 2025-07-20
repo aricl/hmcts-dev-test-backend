fccf20be8bed2da4bd4e1016f6c184f450bdc5a0
Adam John Ready <worldelephantseal@gmail.com>
2025-07-20 14:21:54 +0100

    Get create task request working

--- src/main/java/uk/gov/hmcts/reform/dev/models/Task.java
+++ src/main/java/uk/gov/hmcts/reform/dev/models/Task.java
@@ -27,18 +27,4 @@ public class Task {
 
     @Column(name = "due_date")
     private LocalDateTime due_date;
-
-    public Task(String title, String description, String status) throws Exception {
-        if (title.isEmpty()) {
-            throw new Exception("Title cannot be null");
-        }
-        if (status.isEmpty()) {
-            throw new Exception("Status cannot be null");
-        }
-
-        this.title = title;
-        this.description = description;
-        this.status = status;
-        this.due_date = LocalDateTime.now();
-    }
 }
