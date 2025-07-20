8677f7c4affe7430f284f7b9ee6a0899c0643743
Adam John Ready <worldelephantseal@gmail.com>
2025-07-20 14:48:46 +0100

    Get by id working

--- src/main/java/uk/gov/hmcts/reform/dev/controllers/TaskController.java
+++ src/main/java/uk/gov/hmcts/reform/dev/controllers/TaskController.java
@@ -2,9 +2,7 @@ package uk.gov.hmcts.reform.dev.controllers;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import uk.gov.hmcts.reform.dev.TaskRepository;
 import uk.gov.hmcts.reform.dev.models.Task;
 
@@ -16,10 +14,15 @@ public class TaskController {
         this.taskRepository = taskRepository;
     }
 
-    // insert a product into database
     @PostMapping("/task")
     public ResponseEntity<Task> createTask(@RequestBody Task task) {
         Task savedTask = taskRepository.save(task);
         return new ResponseEntity<>(savedTask, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/task/{id}")
+    public ResponseEntity<Task> getTask(@PathVariable String id) {
+        Task task = taskRepository.findById(Long.parseLong(id)).get();
+        return new ResponseEntity<>(task, HttpStatus.OK);
     }
 }
